58baf4d293dccecdcea95ba8f0cf268072655a84
IsmaryAlvarez <ismaryad@gmail.com>
2018-03-20 12:01:13 -0300

    subiendo renderizado

--- static/js/apps/main.jsx
+++ static/js/apps/main.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React,  { Component } from "react";
+import ReactDOM from "react-dom";
+import {catalog} from './../../../data/mock.json'
 
 const App = ({ message }) => <div>{ message }</div>;
 
-ReactDOM.render(<App message="hello world" />, document.getElementById('app'));
+const ListItem = ({imageURL, name, currency, price}) =>
+catalog.map((data) =>
+<div className="producto">
+ <img src={ data.imageURL } width="150px" height="150px" alt="placeholder+image"/>
+  <p>{data.name}</p>
+  <p>{data.currency}{data.price}</p>
+  <button>Add to Cart</button>
+</div>
+);
+
+
+
+
+ReactDOM.render(<ListItem/>, document.getElementById("Productos"));
+ReactDOM.render(<App message="Product List" />, document.getElementById("app"));
+
